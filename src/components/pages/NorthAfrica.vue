ac0da3ba9f3db7dd572f3600586180fce53c61bd
flishl1 <kirill15022005@gmail.com>
2025-04-28 01:37:26 +0300

    Fix all pages districts russia

--- src/components/pages/NorthAfrica.vue
+++ src/components/pages/NorthAfrica.vue
@@ -1,18 +1,18 @@
 <template>
 	<div class="main">
 		<div class="banner">
-			<h1 class="banner__headding">Северная Африка</h1>
+			<h1 class="banner__heading">Северная Африка</h1>
 		</div>
 		<div class="list">
 			<div class="wrapper">
 				<div class="list__top top">
 					<span class="top__line-red line-red"></span>
-					<p class="top__text text-headdind">СТРАНЫ СЕВЕРНОЙ АФРИКИ</p>
+					<p class="top__text text-heading">СТРАНЫ СЕВЕРНОЙ АФРИКИ</p>
 					<div class="container__card">
 						<div class="item item_1">
 							<img class="card__img" src="@/assets/Flag_of_Algeria.svg" alt="Фотография флага" />
 							<div class="item__card">
-								<p class="card__headding">Алжирская Народная Демократическая Республика</p>
+								<p class="card__heading">Алжирская Народная Демократическая Республика</p>
 								<p class="card__text text-population">Население: 46 700 тыс. чел.</p>
 								<p class="card__text text-square">Площадь: 2 381,741 тыс. кв. км</p>
 								<p class="card__text text-time">Часовой пояс: CET (UTC+1)</p>
@@ -21,7 +21,7 @@
 						<div class="item item_2">
 							<img class="card__img" src="@/assets/Flag_of_Egypt.svg" alt="Фотография флага" />
 							<div class="item__card">
-								<p class="card__headding">Арабская Республика Египет</p>
+								<p class="card__heading">Арабская Республика Египет</p>
 								<p class="card__text text-population">Население: 106 221,67 тыс. чел.</p>
 								<p class="card__text text-square">Площадь: 1 001,45 тыс. кв. км</p>
 								<p class="card__text text-time">Часовой пояс: EET (UTC+2)</p>
@@ -30,7 +30,7 @@
 						<div class="item item_3">
 							<img class="card__img" src="@/assets/Flag_of_Libya.png" alt="Фотография флага" />
 							<div class="item__card">
-								<p class="card__headding">Государство Ливия</p>
+								<p class="card__heading">Государство Ливия</p>
 								<p class="card__text text-population">Население: 7 361,263 тыс. чел.</p>
 								<p class="card__text text-square">Площадь: 1 759,54 тыс. кв. км</p>
 								<p class="card__text text-time">Часовой пояс: EET (UTC+2)</p>
@@ -39,7 +39,7 @@
 						<div class="item item_4">
 							<img class="card__img" src="@/assets/Flag_of_Mauritania.svg" alt="Фотография флага" />
 							<div class="item__card">
-								<p class="card__headding">Исламская Республика Мавритания</p>
+								<p class="card__heading">Исламская Республика Мавритания</p>
 								<p class="card__text text-population">Население: 4 328,04 тыс. чел.</p>
 								<p class="card__text text-square">Площадь: 1 030,7 тыс. кв. км</p>
 								<p class="card__text text-time">Часовой пояс: GMT (UTC+0)</p>
@@ -48,7 +48,7 @@
 						<div class="item item_5">
 							<img class="card__img" src="@/assets/Flag_of_Morocco.svg" alt="Фотография флага" />
 							<div class="item__card">
-								<p class="card__headding">Королевство Марокко</p>
+								<p class="card__heading">Королевство Марокко</p>
 								<p class="card__text text-population">Население: 36 828,33 тыс. чел.</p>
 								<p class="card__text text-square">Площадь: 446 550 тыс. кв. км</p>
 								<p class="card__text text-time">Часовой пояс: GMT (UTC+0)</p>
@@ -57,7 +57,7 @@
 						<div class="item item_6">
 							<img class="card__img" src="@/assets/Flag_of_Sudan.png" alt="Фотография флага" />
 							<div class="item__card">
-								<p class="card__headding">Республика Судан</p>
+								<p class="card__heading">Республика Судан</p>
 								<p class="card__text text-population">Население: 50 467,278 тыс. чел.</p>
 								<p class="card__text text-square">Площадь: 1 886,068 тыс. кв. км</p>
 								<p class="card__text text-time">Часовой пояс: EET (UTC+2)</p>
@@ -66,7 +66,7 @@
 						<div class="item item_7">
 							<img class="card__img" src="@/assets/Flag_of_the_Sahrawi_Arab_Democratic_Republic.png" alt="Фотография флага" />
 							<div class="item__card">
-								<p class="card__headding">Сахарская Арабская Демократическая Республика</p>
+								<p class="card__heading">Сахарская Арабская Демократическая Республика</p>
 								<p class="card__text text-population">Население: 607,292 тыс. чел.</p>
 								<p class="card__text text-square">Площадь: 266 тыс. кв. км</p>
 								<p class="card__text text-time">Часовой пояс: GMT (UTC+0)</p>
@@ -75,7 +75,7 @@
 						<div class="item item_8">
 							<img class="card__img" src="@/assets/Flag_of_Tunisia.svg" alt="Фотография флага" />
 							<div class="item__card">
-								<p class="card__headding">Тунисская Республика</p>
+								<p class="card__heading">Тунисская Республика</p>
 								<p class="card__text text-population">Население: 11 708,37 тыс. чел.</p>
 								<p class="card__text text-square">Площадь: 163,61 тыс. кв. км</p>
 								<p class="card__text text-time">Часовой пояс: CET (UTC+1)</p>
@@ -130,7 +130,7 @@ p {
 	align-items: center;
 }
 
-.banner__headding {
+.banner__heading {
 	font-size: 80px;
 	font-weight: bold;
 	color: #ffffff;
@@ -151,7 +151,7 @@ p {
 	background-color: #aa0000;
 }
 
-.text-headdind {
+.text-heading {
 	margin-bottom: 30px;
 	margin-top: 24px;
 	font-size: 24px;
@@ -180,7 +180,7 @@ p {
 	width: auto;
 }
 
-.card__headding {
+.card__heading {
 	font-size: 27px;
 }
 
@@ -208,7 +208,7 @@ p {
 		justify-content: center;
 	}
 
-	.banner__headding {
+	.banner__heading {
 		font-size: 60px;
 	}
 
@@ -216,13 +216,13 @@ p {
 		flex-direction: column;
 	}
 
-	.card__headding {
+	.card__heading {
 		margin-bottom: 0;
 	}
 }
 
 @media screen and (width < 1000px) {
-	.banner__headding {
+	.banner__heading {
 		font-size: 45px;
 	}
 }
